50fd1c416d53f6a984f87cf2a84e71c2fefc03ae
mcnakhaee <mcnakhaee@gmail.com>
2019-12-28 13:09:13 +0100

    rm

--- temp.R
+++ temp.R
@@ -11,12 +11,7 @@ library(hrbrthemes)
 library(ggtext)
 library(ggdark)
 
-token <- create_token(
-  app = "my_twitter_research_app",
-  consumer_key = 'RgivABxfsX9ubdhxLxZBBh5m7',
-  consumer_secret = '5TigMInpBwJLNnFed8x8JlfNpvjMU0nOd9u1UPgRA2KOYIsNVU',
-  access_token = '496163705-nylSebOzfa5bThbRQHmS40f3eLV0X2z6V514uxcB',
-  access_secret = 'OkTZpcW9CJPMuApCQvWpZAlKR38vz8d2g9UStFDX32U3k')
+
 
 tag = 'صدرالساداتی'
 tweets_fazel<- search_tweets(
